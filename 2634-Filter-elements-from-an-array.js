5f2a98f6743781b9a3c2534c3bc32630a90f3650
Noobie-001 <bhatt5.piyush@gmail.com>
2026-03-02 10:52:52 +0530

    Add solution for 2634 - Filter Elements from Array

---
+++ 2634-Filter-elements-from-an-array.js
@@ -0,0 +1,13 @@
+// 2634. Filter Elements from Array
+
+var filter = function(arr, fn) {
+    let filteredArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+
+    return filteredArr;
+};
