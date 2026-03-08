6b874ddc2f6bf4816cbc09adcc8a78a1ef6d35d4
Noobie-001 <bhatt5.piyush@gmail.com>
2026-03-09 00:32:22 +0530

    2722

---
+++ 2722-join-two-arrays-by-id.js
@@ -0,0 +1,15 @@
+var join = function(arr1, arr2) {
+    const map={}
+    for (obj of arr1){
+        map[obj.id]=obj
+    }
+    for (obj of arr2){
+        if(map[obj.id]){
+            map[obj.id]={...map[obj.id],...obj}
+        }else{
+            map[obj.id]=obj
+        }
+    }
+    return Object.values(map).sort((a,b)=>a.id-b.id);
+
+};
