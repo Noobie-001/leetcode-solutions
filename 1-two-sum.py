64a1b2857a4d10bf70a03f329e96d7d1f430a0fb
Noobie-001 <bhatt5.piyush@gmail.com>
2026-04-13 15:56:22 +0530

    1-two-sum

---
+++ 1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d={}
+        for i in range(len(nums)):
+            
+            complement=target-nums[i]
+            if complement in d:
+                return([d[complement], i])
+            d[nums[i]] = i
