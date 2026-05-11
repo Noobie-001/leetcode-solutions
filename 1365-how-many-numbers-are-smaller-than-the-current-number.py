f37c1209ab94fc1bf2bf30ebd62c28b3338a2fb4
Noobie-001 <bhatt5.piyush@gmail.com>
2026-05-11 07:52:42 +0530

    2520

--- 1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -7,4 +7,16 @@ class Solution:
                     countSmallNums[i]+=1
         return countSmallNums
 
-        
+#better approach not best 
+
+class Solution:
+     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in nums:
+            c = 0
+            for j in nums:
+                if j<i:
+                    c+=1
+            ans.append(c)
+
+        return ans
---
+++ 2510-count-the-digits-that-divide-a-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        count=0
+        temp=num
+        while temp>0:
+            r=temp%10
+            if num%r==0:
+                count+=1
+            temp//=10
+        return count
