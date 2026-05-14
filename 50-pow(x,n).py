2bca00dd6abb69c8b8a0e82ad66c1a616a89fade
Noobie-001 <bhatt5.piyush@gmail.com>
2026-05-15 01:40:14 +0530

    50-updated

--- 50-pow(x,n).py
+++ 50-pow(x,n).py
@@ -6,3 +6,22 @@ class Solution:
             return self.myPow(1/x,-n)
         return x*self.myPow(x,n-1)
         
+
+//better approach
+class Solution:
+    def findPow(self,x,n):
+        if x==0:
+            return 1
+        a=self.findPow(x,n//2)
+        if n%2==0:
+            return a*a
+        else:
+            return a*a*x 
+
+    def myPow(self, x: float, n: int) -> float:
+        if n>0:
+            return self.findPow(x,n)
+        if n<0:
+            return self.myPow(1/x,-n )
+        return x*self.myPow(x,n-1)
+        
