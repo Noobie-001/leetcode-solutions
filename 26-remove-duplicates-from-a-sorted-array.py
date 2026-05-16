966583bf94ca44a979df64815097d69bdb25761b
Noobie-001 <bhatt5.piyush@gmail.com>
2026-05-16 10:14:42 +0530

    25 and 80

--- 26-remove-duplicates-from-a-sorted-array.py
+++ 26-remove-duplicates-from-a-sorted-array.py
@@ -8,16 +8,18 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] not in expectedNums:
                 expectedNums.append(nums[i])
-        return expectedNums
+            nums[:]=expectedNums
+        return len(nums)
         
 #better approach
 class Solution: 
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        k=1
+        start=0
         for i in range (1,len(nums)):
-            if nums[i]!=nums[i-1]:
-                nums[k]=nums[i]
-                k+=1
-        return k
+            if nums[i]!=nums[start-1]:
+                nums[start]=nums[i]
+                start+=1
+        return start+1   
+    
