99c19e414d0ec812ff1f9359c38e2facd2d6a6c3
Noobie-001 <bhatt5.piyush@gmail.com>
2026-05-22 08:43:51 +0530

    344 second method

--- 344-reverse-string.py
+++ 344-reverse-string.py
@@ -2,3 +2,14 @@ class Solution:
     def reverseString(self, s: List[str]) -> None:
         s[:]=s[::-1]
          
+#what leetcode wants 
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right=0, len(s)-1
+        while left<right:
+            s[left], s[right]= s[right], s[left]
+            left+=1
+            right-=1
+        
+         
