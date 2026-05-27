b97c356d2c894daa54896ddaae108f699023b29f
Noobie-001 <bhatt5.piyush@gmail.com>
2026-05-28 01:16:40 +0530

    1-unoptimised-approach

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -7,3 +7,11 @@ class Solution(object):
             if complement in d:
                 return([d[complement], i])
             d[nums[i]] = i
+
+#my unoptimised approach class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        count=0
+        for i in range(len(nums)):
+            count=target-nums[i]
+            if count in nums[i+1:]:
+                indices=[i,nums.index((count),i+1)]
